1be137d3e057ee8b2003335bf14ab305895cc169
[jwhong70] <[jwhong70@gmail.com]>
2022-11-15 22:00:59 +0900

    #1-4 Theme, GlobalStyles

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 import { useReactiveVar } from "@apollo/client";
-import { isLoggedInVar } from "./apollo";
+import { isLoggedInVar, darkModeVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const lightTheme = {
+  accent: "#0095f6",
+  bgColor: "#FAFAFA",
+  fontColor: "rgb(38, 38, 38)",
+  borderColor: "rgb(219, 219, 219)",
+};
+export const darkTheme = {
+  accent: "#0095f6",
+  bgColor: "#000000",
+  fontColor: "white",
+  borderColor: "rgb(219,219,219)",
+};
+export const GlobalStyles = createGlobalStyle`
+  ${reset}
+  input { all:unset; }
+  * { box-sizing:border-box; }
+  body { 
+    background-color:${(props) => props.theme.bgColor};
+    font-size:14px; font-family:'Open Sans', sans-serif; 
+    color:${(props) => props.theme.fontColor};
+    }
+  a { text-decoration: none; color:inherit; }
+`;
