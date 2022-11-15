d0da750eae47b4871df25fb81e7b3f4514cfc6a4
[jwhong70] <[jwhong70@gmail.com]>
2022-11-15 20:24:41 +0900

    #1-2 Page Routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+
 function App() {
-  return null;
+  const isLoggedIn = false;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
